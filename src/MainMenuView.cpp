ea413fca29144f0c2d2f750e5c19e62b2031165a
vrameau <valentin.rameau@epitech.eu>
2017-12-30 08:48:25 +0100

    remove text printing

--- src/MainMenuView.cpp
+++ src/MainMenuView.cpp
@@ -21,13 +21,11 @@ void MainMenuView::render(const MainMenuModel* model) {
     vita2d_draw_rectangle(680, 350, 100, 150, RGBA8(0, 0, 255, 255));
     vita2d_draw_fill_circle(200, 420, 100, RGBA8(0, 255,0 ,255));
 
-    // vita2d_draw_texture(image, x, y);
-
     vita2d_draw_line(500, 30, 800, 300, RGBA8(255, 0, 255, 255));
 
-    vita2d_pgf_draw_text(this->_pgf, 700, 30, RGBA8(0,255,0,255), 1.0f, "PGF Font sample!");
+    // vita2d_pgf_draw_text(this->_pgf, 700, 30, RGBA8(0,255,0,255), 1.0f, "PGF Font sample!");
 
-    vita2d_pvf_draw_text(this->_pvf, 700, 80, RGBA8(0,255,0,255), 1.0f, "PVF Font sample!");
+    // vita2d_pvf_draw_text(this->_pvf, 700, 80, RGBA8(0,255,0,255), 1.0f, "PVF Font sample!");
 
     for (int i = 0; i < model->getButtons()->size(); i++)
     {
--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,6 @@
 
 int main()
 {
-	// psvDebugScreenInit();
 	// Key key;
 	// SceCtrlData pad;
 	// memset(&pad, 0, sizeof(pad));
@@ -43,20 +42,44 @@ int main()
 	// 	sceKernelDelayThread(1000 * 100);
 	// }
 
+
+
+	psvDebugScreenInit();
 	vita2d_init();
-
+    vita2d_pgf *pgf;
+    pgf = vita2d_load_default_pgf();
 	globals::globalState = globals::STATE_RUN;
-
+	unsigned long long a = 0;
+    std::string b;
+	// while (1)
+	// {
+    //     globals::sceneManager.load(ISceneControler::MAIN_MENU);
+    //     if (globals::sceneManager.getNewSceneControler())
+    //         ((MainMenuControler*)globals::sceneManager.getNewSceneControler())->init();
+	// 	globals::sceneManager.clean();
+	// 	sceKernelDelayThread(1000 * 100);
+	// }
 	globals::sceneManager.load(ISceneControler::SPLASH_SCREEN);
 	while (globals::globalState == globals::STATE_RUN)
 	{
-		 globals::sceneManager.getSceneControler()->handleInput();
+		globals::sceneManager.getSceneControler()->handleInput();
+		globals::sceneManager.load(ISceneControler::MAIN_MENU);
+        if (globals::sceneManager.getNewSceneControler())
+            ((MainMenuControler*)globals::sceneManager.getNewSceneControler())->init();
 		 globals::sceneManager.clean();
+    	b = std::to_string(a++);
+		vita2d_start_drawing();
+		vita2d_clear_screen();
+    	vita2d_pgf_draw_text(pgf, 400, 400, RGBA8(0,255,0,255), 1.0f, b.c_str());
+		vita2d_end_drawing();
+		vita2d_swap_buffers();
+		sceKernelDelayThread(1000 * 100);
 	}
 	/*
 	 * vita2d_fini() waits until the GPU has finished rendering,
 	 * then we can free the assets freely.
 	 */
+    vita2d_free_pgf(pgf);
 	vita2d_fini();
 
 	sceKernelExitProcess(0);
