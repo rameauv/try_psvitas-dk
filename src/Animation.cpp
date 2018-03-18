3e6e6c3c2658cdf0987ece816d7997f07dea3517
vrameau <valentin.rameau@epitech.eu>
2018-03-18 20:39:17 +0100

    Animation => rename class private attributes

--- src/Animation.cpp
+++ src/Animation.cpp
@@ -5,26 +5,26 @@ int Animation::load(const vita2d_texture* texture, int duration, int col, int li
     _duration = duration;
     _col = col;
     _line = line;
-    _sprite_width = vita2d_texture_get_width(texture) / col;
-    _sprite_height = vita2d_texture_get_width(texture) / line;
+    _spriteWidth = vita2d_texture_get_width(texture) / col;
+    _spriteHeight = vita2d_texture_get_width(texture) / line;
 }
 
 void Animation::draw(float x, float y, float x_scale, float y_scale, float rotate) {
     if (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - _lastAnimTime).count() < _duration) {
-        vita2d_draw_texture_part_scale_rotate(_texture, x, y, _current_col * _sprite_width, _current_line * _sprite_height, _sprite_width, _sprite_height, x_scale, y_scale, rotate);
+        vita2d_draw_texture_part_scale_rotate(_texture, x, y, _currentCol * _spriteWidth, _currentLine * _spriteHeight, _spriteWidth, _spriteHeight, x_scale, y_scale, rotate);
         return;
     }
     _lastAnimTime = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now());
-    _current_col++;
-    if (_current_col > _col) {
-        _current_col = 0;
-        _current_line++;
+    _currentCol++;
+    if (_currentCol > _col) {
+        _currentCol = 0;
+        _currentLine++;
     }
-    if (_current_line > _line) {
-        _current_col = 0;
-        _current_line = 0;
+    if (_currentLine > _line) {
+        _currentCol = 0;
+        _currentLine = 0;
     }
-    vita2d_draw_texture_part_scale_rotate(_texture, x, y, _current_col * _sprite_width, _current_line * _sprite_height, _sprite_width, _sprite_height, x_scale, y_scale, rotate);
+    vita2d_draw_texture_part_scale_rotate(_texture, x, y, _currentCol * _spriteWidth, _currentLine * _spriteHeight, _spriteWidth, _spriteHeight, x_scale, y_scale, rotate);
 }
 
 const vita2d_texture* Animation::getTexture() const {
